ca1ab56cc649ca85f725c590f8ee88209630c962
Eduardo <eduardo_muller0301@hotmail.com>
2020-10-06 00:22:49 -0300

    NaverList

--- nave/src/components/home/NaversList.jsx
+++ nave/src/components/home/NaversList.jsx
@@ -1,19 +1,33 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import { FaPen, FaTrash } from "react-icons/fa";
+import api from "../../api/Api"
 import { Link } from "react-router-dom"
 
-export default (props) => {
-  props.naversList.map((naver, i) => {
-    return (
-      <div className="col-sm-3 m-4 c4" key={i}>
-        <div className="d-flex justify-content-center ">
-          <Link to={`/navers/${naver._id}`}>
-            <img src={naver.imgPath} alt={`Naver${i}`} />
-          </Link>
-        </div>
-        <h5>{naver.name}</h5>
-        <h5>{naver.title}</h5>
-      </div>
+export default () => {
 
-    )
-  })
+  const [navers, setNavers] = useState([])
+  const [id, setId] = useState("")
+  const [load, setLoad] = useState(false)
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    let isMounted = true;
+    api.get("navers")
+      .then(res => {
+        if (isMounted) {
+          setNavers(res.data);
+          setId([...id, ...res.data]);
+          setLoad(true);
+        }
+      })
+      .catch(err => {
+        setError(err.message);
+        setLoad(true);
+      })
+
+    return () => {
+      isMounted = false;
+    }
+  }, [id]);
+
 }
